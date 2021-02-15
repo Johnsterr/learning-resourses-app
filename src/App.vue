4e14308d6850ffc4d6e55cd63966a1bb0410f981
Evgeniy <evgpashko92@gmail.com>
2021-02-15 23:58:42 +0700

    Create App.vue
    Add some data

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<ul></ul>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			storedResources: [
+				{
+					id: 'off-guide',
+					title: 'Официальное руководство',
+					description: 'Официальная документация фреймворка VueJS',
+					link: 'https://vuejs.org/'
+				},
+				{
+					id: 'yand',
+					title: 'Yandex',
+					description: 'Поисковая система Яндекс',
+					link: 'https://yandex.ru/'
+				}
+			]
+		}
+	}
+}
+</script>
