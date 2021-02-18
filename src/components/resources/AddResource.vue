32327218bbce74d40a3c92eb14dab776ebfe06cc
Evgeniy <evgpashko92@gmail.com>
2021-02-18 19:52:32 +0700

    Adding a Form
    Add Form and Styles, change BaseButton props

--- src/components/resources/AddResource.vue
+++ src/components/resources/AddResource.vue
@@ -1,3 +1,47 @@
 <template>
-	<h2>Добавить ресурс</h2>
+	<base-card>
+		<form>
+			<div class="form-control">
+				<label for="title">Название</label>
+				<input id="title" name="title" type="text">
+			</div>
+			<div class="form-control">
+				<label for="description">Описание</label>
+				<textarea id="description" name="description" rows="4"></textarea>
+			</div>
+			<div class="form-control">
+				<label for="link">Ссылка</label>
+				<input id="link" name="link" type="url">
+			</div>
+			<div>
+				<base-button type="submit">Добавить ресурс</base-button>
+			</div>
+		</form>
+	</base-card>
 </template>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input, textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus, textarea:focus {
+  outline: none;
+  border-color: #f8a13e;
+  background-color: #fdeeda;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
